a867eb9f0f71a13d0dcfdb6a48072db874a045fe
Nikita Tarasov <nikitatarasov987@gmail.com>
2020-11-07 17:44:03 +0400

    Russian roulette app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,111 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="screen">
+    <carousel :cells="cells"
+              :rotateStyle="rotateStyle"
+    ></carousel>
+    <button class="button"
+            @click="load"
+    >Load{{loadedBullets}}</button>
+    <button class="button"
+            v-on:click="rotateCarousel"
+    >Rotate</button>
+    <button class="button"
+            @click="fire"
+    >Fire!</button>
+      <div v-if="message">{{message}}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import carousel from "@/components/carousel";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data: function() {
+        return {
+            cells: new Array(6).fill('?'),
+            selectedIndex: 0,
+            position: 0,
+            bullets: 0,
+            cylinder: new Array(6).fill(0),
+            message: '',
+            rotateStyleInit: {
+                'transform': null,
+            }
+        }
+    },
+    methods: {
+        getRandomInt: function(min, max) {
+            return Math.floor(min + Math.random()*(max + 1 - min));
+        },
+        rotateCarousel: function() {
+            this.clearMessage();
+            this.selectedIndex++;
+            if(this.position < 6) {
+                this.position++;
+            } else {
+                this.position = 0;
+            }
+            //let randRounds =  this.getRandomInt(1, Math.round(this.cells.length/2));
+            let angle = this.selectedIndex / this.cells.length * -360 ;
+            this.rotateStyle.transform = `translateZ(-199px) rotateY(${angle}deg)`;
+        },
+        load: function() {
+            this.clearMessage();
+            if(this.bullets < 5) {
+                if(!this.cylinder[this.position]) {
+                    this.cylinder[this.position] = 1;
+                    this.bullets++;
+                } else {
+                    this.message = 'This chamber is already full!';
+                }
+            }
+        },
+        clearMessage: function() {
+            this.message = '';
+        },
+        fire: function() {
+            this.bullets = 0;
+            this.position = 0;
+            this.cylinder.fill(0);
+            if (this.cylinder[this.position]) {
+                this.message = 'You are dead!';
+            } else {
+                this.message = 'You are lucky bastard!';
+            }
+        }
+    },
+    computed: {
+        rotateStyle: {
+            get: function() {
+                return this.rotateStyleInit;
+            },
+            set: function (newValue) {
+                let newRotateClass = Object.assign({}, this.rotateStyleInit);
+                newRotateClass.transform = newValue;
+                this.rotateStyleInit = newRotateClass;
+            }
+        },
+        loadedBullets: function() {
+            return this.bullets ? `(${this.bullets})` : '';
+        }
+    },
+    components: {
+        carousel,
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .screen {
+    display: grid;
+    grid: 250px 50px 50px 50px / 100%;
+    grid-row-gap: 10px;
+    justify-content: center;
+  }
+
+  .button {
+      justify-self: center;
+      width: 150px;
+  }
 </style>
---
+++ src/components/carousel.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="scene">
+        <div class="carousel"
+             v-bind:style="[rotateStyle.transform ? rotateStyle : '']">
+            <div class="carousel_cell"
+                v-for="(cell, index) in cells"
+                v-bind:key="index"
+            >{{cell}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "carousel",
+        props: {
+            cells: Array,
+            rotateStyle: Object,
+        }
+    }
+</script>
+
+<style scoped>
+    .scene {
+        border: 1px solid #CCC;
+        position: relative;
+        width: 230px;
+        height: 170px;
+        margin: 50px auto;
+        perspective: 1000px;
+    }
+
+    .carousel {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        transform: translateZ(-199px);
+        transform-style: preserve-3d;
+        transition: transform 1s;
+    }
+
+    .carousel_cell {
+        position: absolute;
+        width: 205px;
+        height: 140px;
+        margin: 10px;
+        border: 2px solid black;
+        line-height: 116px;
+        font-size: 80px;
+        font-weight: bold;
+        color: white;
+        text-align: center;
+    }
+
+    .carousel_cell:nth-child(6n+1) { background: hsla(  0, 100%, 50%, 0.8); }
+    .carousel_cell:nth-child(6n+2) { background: hsla( 30, 100%, 50%, 0.8); }
+    .carousel_cell:nth-child(6n+3) { background: hsla( 60, 100%, 50%, 0.8); }
+    .carousel_cell:nth-child(6n+4) { background: hsla( 90, 100%, 50%, 0.8); }
+    .carousel_cell:nth-child(6n+5) { background: hsla( 160, 100%, 50%, 0.8); }
+    .carousel_cell:nth-child(6n+0) { background: hsla( 200, 100%, 50%, 0.8); }
+
+    .carousel_cell:nth-child(1) { transform: rotateY(  0deg) translateZ(199px); }
+    .carousel_cell:nth-child(2) { transform: rotateY( 60deg) translateZ(199px); }
+    .carousel_cell:nth-child(3) { transform: rotateY( 120deg) translateZ(199px); }
+    .carousel_cell:nth-child(4) { transform: rotateY( 180deg) translateZ(199px); }
+    .carousel_cell:nth-child(5) { transform: rotateY( 240deg) translateZ(199px); }
+    .carousel_cell:nth-child(6) { transform: rotateY( 300deg) translateZ(199px); }
+</style>
